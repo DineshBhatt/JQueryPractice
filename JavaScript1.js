a1fca8bff81be312f0d19e3229d2531666c73249
DineshBhatt <42347016+DineshBhatt@users.noreply.github.com>
2018-09-19 09:51:34 +0530

    Add files via upload

---
+++ JavaScript1.js
@@ -0,0 +1,16 @@
+﻿//alert("welcome t o jquery");
+console.log("hello world");
+
+
+$(document).ready(FirstMethod);
+
+function FirstMethod(){
+    $("body").css("background", "green");
+    $(".firsty").css("background", "blue");
+    $(".table-form").css("background", "skyblue");
+    if (submit != null) {
+        alert("enter sumk data inside");
+    } else {
+        alert("the data is inserted in the database");
+    }
+} 
